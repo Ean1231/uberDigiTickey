a953b36e8dd52bdb6444af3225d86b1d096fcaee
Ean1231 <macdonaldbosman@gmail.com>
2021-08-26 09:43:08 +0200

    Mackey

--- src/app/driver-register/driver-register.page.ts
+++ src/app/driver-register/driver-register.page.ts
@@ -11,8 +11,17 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./driver-register.page.scss'],
 })
 export class DriverRegisterPage implements OnInit {
+  showmessage
+  name
+  surname
+  email
+  confirmPassword
+  Date
+  password
+  path: string;
+  IDimg
+  phoneNumber;
 
-data: any;
   constructor(public router: Router,
     public auth:AuthenticationService,
     private firestore: AngularFirestore,
@@ -20,9 +29,56 @@ data: any;
     public modalCtrl: ModalController,) { }
 
   ngOnInit() {
-    this.data =this.router.getCurrentNavigation().extras.state;
-     console.log(this.data)
   }
 
+z
+  async presentAlert(message) {
+    const alert = await this.alertController.create({
+      header: 'Attention User',
+      message:message,
+      buttons: [{
+        text:'OK',
+        handler: () => {
+          this.router.navigateByUrl('/login')
+        //  this.dismiss()
+        }
+
+      }]
+    });
+
+    await alert.present();
+
+  }
+
+
+  async dismiss() {
+    return await this.modalCtrl.dismiss();
+  }
+  btnBack(){
+    //this.router.navigate("./home");
+  }
+
+  upload($event) {
+    this.path = $event.target.files[0];
+  }
+
+  getImage(event) {
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
+
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.IDimg = reader.result;
+
+
+      };
+
+
+      // ChangeDetectorRef since file is loading outside the zone
+
+    }
+  }
 
 }
